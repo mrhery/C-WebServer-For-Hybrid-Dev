f92b45117f858b216a5f6bc3f2591931f6650ca3
Mr Hery <heryintelt@gmail.com>
2022-03-06 17:27:57 +0800

    first init

---
+++ CSharpWebServer/CSharpWebServer/CSWebServer.cs
@@ -0,0 +1,53 @@
+﻿using System.Net;
+using System.Text;
+
+namespace CSharpWebServer
+{
+    public class CSWebServer
+    {
+        public string DocumentRoot { get; set; }
+        public string index = "index.html";
+        public string Address { get; set; }
+
+        private HttpListener Server;
+
+        public CSWebServer()
+        {
+            Server = new HttpListener();
+        }
+
+        public void Start()
+        {
+            if (string.IsNullOrEmpty(Address))
+            {
+                throw new Exception("Address cannot be empty.");
+            }
+
+            if (string.IsNullOrEmpty(DocumentRoot))
+            {
+                throw new Exception("DocumentRoot cannot be empty.");
+            }
+
+            if (!Directory.Exists(DocumentRoot))
+            {
+                Directory.CreateDirectory(DocumentRoot);
+            }
+
+            Server.Prefixes.Add(Address.Trim('/') + "/");
+
+            Server.Start();
+            Console.WriteLine("Server is starting: " + Address);
+            while (Server.IsListening)
+            {
+                HttpListenerContext context = (HttpListenerContext)Server.GetContext();
+                HttpListenerResponse response = context.Response;
+                HttpListenerRequest request = context.Request;
+
+                string content = "Welcome!";
+                response.ContentLength64 = content.Length;
+                byte[] buffer = Encoding.ASCII.GetBytes(content);
+                response.OutputStream.Write(buffer, 0, buffer.Length);
+            }
+        }
+    }
+}
---
+++ CSharpWebServer/WindowsSample/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using CSharpWebServer;
+
+
+CSWebServer server = new CSWebServer();
+
+server.DocumentRoot = Directory.GetCurrentDirectory() + "/www";
+server.Address = "http://127.0.0.1:8888";
+
+server.Start();
